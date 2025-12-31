d5ac2dbc054772b38673502ae96a50872fb3de6f
PEJpOhno <pejpohn@gmail.com>
2025-12-31 18:09:32 +0900

    create pyproject.toml

--- src/rxnfit/__init__.py
+++ src/rxnfit/__init__.py
@@ -1,2 +1,6 @@
-from src.rxnfit.rxn_reader import *
-from src.rxnfit.build_ode import *
+from ._version import __version__
+from .rxn_reader import *
+from .build_ode import *
+
+# from src.rxnfit.rxn_reader import *
+# from src.rxnfit.build_ode import *
